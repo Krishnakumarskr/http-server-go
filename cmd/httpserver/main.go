3ca259eb9d426951be70f0497522012e1793b891
Krishna kumar S <krishna291098@gmail.com>
2026-09-07 17:36:46 +0530

    sending binary

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -108,6 +108,15 @@ func main() {
 				return
 			}
 
+		} else if req.RequestLine.RequestTarget == "/video" {
+			f, _ := os.ReadFile("assets/vim.mp4")
+
+			h.Replace("content-type", "video/mp4")
+			h.Replace("content-length", fmt.Sprintf("%d", len(f)))
+
+			w.WriteStatusLine(response.Success)
+			w.WriteHeaders(*h)
+			w.WriteBody(f)
 		}
 
 		h.Replace("content-length", fmt.Sprintf("%d", len(body)))
